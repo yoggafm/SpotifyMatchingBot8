39f1a2a6d2ae492bbb2e8b885672c1bcf63ef823
yoggafm <118955791+yoggafm@users.noreply.github.com>
2022-11-24 16:31:26 +0700

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,7 +1,7 @@
 import os
 
 os.environ['PYTHONPATH'] = os.path.realpath(__name__)
-os.environ['BOT_TOKEN'] = ''
+os.environ['BOT_TOKEN'] = '5728932780:AAFQ-Hz0QAQHIV7j7s8hiH0n6Fj9adfKfhQ'
 os.environ['SPOTIPY_CLIENT_ID'] = ''
 os.environ['SPOTIPY_CLIENT_SECRET'] = ''
 os.environ['SPOTIPY_REDIRECT_URI'] = ''
